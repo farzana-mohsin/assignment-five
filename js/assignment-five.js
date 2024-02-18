c283ad9f8a5a53e1eab5026ccb16d976fba9b16e
Farzana Mohsin <fmohsin@usertesting.com>
2024-02-17 19:58:29 -0500

    add scroll function to ticketing section

--- js/assignment-five.js
+++ js/assignment-five.js
@@ -1,1 +1,10 @@
+// scroll to ticketing section
+const buyTickets = document.getElementById("buyTickets");
+buyTickets.addEventListener("click", scrollToTicket);
 
+function scrollToTicket() {
+  const ticketingSection = document.getElementById("ticketingSection");
+  ticketingSection.scrollIntoView({
+    behavior: "smooth",
+  });
+}
