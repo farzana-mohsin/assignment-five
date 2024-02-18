8e93fb19d433778a448106e8d55028aac66672a6
Farzana Mohsin <fmohsin@usertesting.com>
2024-02-17 20:49:06 -0500

    increase number of seats selected on seat click

--- js/assignment-five.js
+++ js/assignment-five.js
@@ -11,9 +11,9 @@ function scrollToTicket(event) {
 
 // change seatsLeft count on seat click
 const seat = document.getElementById("A1");
-seat.addEventListener("click", changeSeatsLeftCount);
+seat.addEventListener("click", decrementSeatsLeftCount);
 
-function changeSeatsLeftCount(event) {
+function decrementSeatsLeftCount(event) {
   const seatsLeft = getParsedInnerTextById("seats-left");
   const newSeatsLeft = seatsLeft - 1;
   setInnerTextById("seats-left", newSeatsLeft);
@@ -24,4 +24,13 @@ seat.addEventListener("click", changeSelectedSeatColor);
 
 function changeSelectedSeatColor(event) {
   setBackgroundColorByElement(event.target, "bg-green-400");
+}
+
+// increase number of seats selected on seat click
+seat.addEventListener("click", incrementSeatsSelectedCount);
+
+function incrementSeatsSelectedCount(event) {
+  const seatCount = getParsedInnerTextById("seat-added");
+  const newSeatCount = seatCount + 1;
+  setInnerTextById("seat-added", newSeatCount);
 }
